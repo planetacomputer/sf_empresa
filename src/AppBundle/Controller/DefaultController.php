2aaabda9c9b6673adf8a202134721d3098814bd1
planetacomputer <evapardeza123@gmail.com>
2017-05-30 09:51:23 +0200

    Query empleats i empleat

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -22,6 +22,38 @@ class DefaultController extends Controller
      */
     public function empleatsAction(Request $request)
     {
-        return $this->render('default/empleats.html.twig');
+        try{
+            $dbh = $this->databaseConnection();
+
+            $sql = "SELECT * FROM T_EMPLOYEES";
+            $query = $dbh->prepare($sql);
+            $query->execute();
+
+            $results=$query->fetchAll(\PDO::FETCH_OBJ);
+            }
+        catch (PDOException $e){
+            exit("Error: " . $e->getMessage());
+        }
+        return $this->render('default/empleats.html.twig', array('empleats' => $results));
+    }
+
+    /**
+     *  @Route("/empleat/{id}", name="empleat_show", requirements={"id": "\d+"})
+     */
+    public function showEmpleatAction(Request $request)
+    {
+        $id = $request->get('id');
+        $dbh = $this->databaseConnection();
+        $sql = "SELECT * FROM T_EMPLOYEES WHERE EMPL_ID = :id";
+        $query = $dbh->prepare($sql);
+        $query -> bindParam(':id', $id, \PDO::PARAM_INT);
+        $query->execute();
+        $empleat=$query->fetch(\PDO::FETCH_OBJ);
+        return $this->render('default/showEmpleat.html.twig', array('user' => $empleat));
+    }
+
+    private function databaseConnection(){
+        $dbh = new \PDO("mysql:host=".$this->container->getParameter('database_host').";dbname=".$this->container->getParameter('database_name'),$this->container->getParameter('database_user'), $this->container->getParameter('database_password'), array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        return $dbh;
     }
 }
